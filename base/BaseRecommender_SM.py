a65ea9f7002e8886bed1f80e84e158415816647c
yigitozgumus <yigitozgumus1@gmail.com>
2018-10-20 17:19:44 +0200

    normalization is gone for the time being

--- base/BaseRecommender_SM.py
+++ base/BaseRecommender_SM.py
@@ -19,23 +19,23 @@ def recommend(self, playlist_id, n=None, exclude_seen=True):
             scores = user_profile.dot(self.W_sparse).toarray().ravel()
            # print(scores)
         else:
-            user_profile = self.URM_train.indices[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
-            user_ratings = self.URM_train.data[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
+            user_profile = self.URM_train.indices[self.URM_train.indptr[playlist_id]:self.URM_train.indptr[playlist_id + 1]]
+            user_ratings = self.URM_train.data[self.URM_train.indptr[playlist_id]:self.URM_train.indptr[playlist_id + 1]]
             relevant_weights = self.W[user_profile]
             scores = relevant_weights.T.dot(user_ratings)
 
-        if self.normalize:
-            # normalization will keep the scores in the same range
-            # of value of the ratings in dataset
-
-            rated = user_profile.copy()
-            rated.data = np.ones_like(rated.data)
-            if self.sparse_weights:
-                den = rated.dot(self.W_sparse).toarray().ravel()
-            else:
-                den = rated.dot(self.W).ravel()
-            den[np.abs(den) < 1e-6] = 1.0  # to avoid NaNs
-            scores /= den
+        # if self.normalize:
+        #     # normalization will keep the scores in the same range
+        #     # of value of the ratings in dataset
+        #
+        #     rated = user_profile.copy()
+        #     rated.data = np.ones_like(rated.data)
+        #     if self.sparse_weights:
+        #         den = rated.dot(self.W_sparse).toarray().ravel()
+        #     else:
+        #         den = rated.dot(self.W).ravel()
+        #     den[np.abs(den) < 1e-6] = 1.0  # to avoid NaNs
+        #     scores /= den
 
         if exclude_seen:
             scores = self._filter_seen_on_scores(playlist_id, scores)
