d8d8949790da71aaa8ed7ef21c87392b8bfb1fa8
yigitozgumus <yigitozgumus1@gmail.com>
2018-10-20 11:26:42 +0200

    metrics changes

--- base/Metrics.py
+++ base/Metrics.py
@@ -12,18 +12,20 @@ class Metrics(object):
         pass
 
     def precision(self,is_relevant):
-        precision_score = np.sum(is_relevant, dtype=np.float32) / len(is_relevant)
+        #is_relevant = np.in1d(recommended_items, relevant_items, assume_unique=True)
+        precision_score = np.sum(is_relevant, dtype=np.bool) / len(is_relevant)
         assert 0 <= precision_score <= 1, precision_score
         return precision_score
 
-    def recall(self,is_relevant,position_items):
-        recall_score = np.sum(is_relevant, dtype=np.float32)/ position_items.shape[0]
+    def recall(self,is_relevant, relevant_items):
+        recall_score = np.sum(is_relevant, dtype=np.bool)/ relevant_items.shape[0]
         assert 0 <= recall_score <= 1, recall_score
         return recall_score
 
-    def map(self,is_relevant,position_items):
+    def map(self,is_relevant, relevant_items):
+        #is_relevant = np.in1d(recommended_items, relevant_items, assume_unique=True)
         p_at_k = is_relevant * np.cumsum(is_relevant,dtype=np.float32)\
                  / (1 + np.arange(is_relevant.shape[0]))
-        map_score = np.sum(p_at_k) / np.min([position_items.shape[0], is_relevant.shape[0]])
+        map_score = np.sum(p_at_k) / np.min([relevant_items.shape[0], is_relevant.shape[0]])
         assert 0 <= map_score <= 1, map_score
         return map_score
