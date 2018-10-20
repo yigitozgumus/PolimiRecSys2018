fe841c56887e915102deebe2a4b31afa9d0b26f5
yigitozgumus <yigitozgumus1@gmail.com>
2018-10-20 11:29:15 +0200

    User based CF with KNN is completed

--- models/KNN/User_KNN_CFRecommender.py
+++ models/KNN/User_KNN_CFRecommender.py
@@ -8,21 +8,30 @@ from base.RecommenderUtils import check_matrix
 
 class UserKNNCFRecommender(RecommenderSystem, RecommenderSystem_SM):
 
-    def __init__(self, URM_train, sparse_weights=True):
+    def __init__(self, URM_train, sparse_weights=True, verbose=False, similarity_mode="cosine"):
         super(UserKNNCFRecommender, self).__init__()
 
         self.URM_train = check_matrix(URM_train, 'csr')
         self.sparse_weights = sparse_weights
+        self.verbose = verbose
+        self.similarity_mode = similarity_mode
 
-    def fit(self, k=50, shrink=100, similarity='cosine', normalize=True):
+    def __str__(self):
+        representation = "User User KNN Collaborative Filter Class with parameters: " + \
+        "sparse_weights= {0}, verbose= {1}, similarity= {2}".format(self.sparse_weights,
+                                                                    self.verbose,
+                                                                    self.similarity_mode)
+        return representation
+
+    def fit(self, k=50, shrink=100, normalize=True):
         self.k = k
         self.shrink = shrink
         self.normalize = normalize
-        self.similarity = Similarity(self.URM_train.T,
+        self.similarity = Similarity(self.URM_train,
                                      shrink=shrink,
+                                     verbose=self.verbose,
                                      neighbourhood=k,
-                                     normalize=normalize,
-                                     mode=similarity)
+                                     mode=self.similarity_mode)
 
         if self.sparse_weights:
             self.W_sparse = self.similarity.computeUUSimilarity()
@@ -31,35 +40,21 @@ class UserKNNCFRecommender(RecommenderSystem, RecommenderSystem_SM):
             self.W = self.W.toarray()
 
     def recommend(self, playlist_id, n=None, exclude_seen=True):
+        # TODO
+        if n == None:
+            n = self.URM_train.shape[1] - 1
 
-            if n == None:
-                n = self.URM_train.shape[1] - 1
+        # compute the scores using the dot product
+        if self.sparse_weights:
+            scores = self.W_sparse[playlist_id].dot(self.URM_train).toarray().ravel()
+        # print(scores)
+        else:
+            scores = self.URM_train.T.dot(self.W[playlist_id])
 
-            # compute the scores using the dot product
-            if self.sparse_weights:
-                scores = self.W_sparse[playlist_id].dot(self.URM_train).toarray().ravel()
-            # print(scores)
-            else:
-                scores = self.URM_train.T.dot(self.W[playlist_id])
+        if exclude_seen:
+            scores = self._filter_seen_on_scores(playlist_id, scores)
 
-            if self.normalize:
-                # normalization will keep the scores in the same range
-                # of value of the ratings in dataset
-                user_profile = self.URM_train[playlist_id]
-                rated = user_profile.copy()
-                rated.data = np.ones_like(rated.data)
-                if self.sparse_weights:
-                    den = rated.dot(self.W_sparse).toarray().ravel()
-                else:
-                    den = rated.dot(self.W).ravel()
-                den[np.abs(den) < 1e-6] = 1.0  # to avoid NaNs
-                scores /= den
-
-            if exclude_seen:
-                scores = self._filter_seen_on_scores(playlist_id, scores)
-
-            relevant_items_partition = (-scores).argpartition(n)[0:n]
-            relevant_items_partition_sorting = np.argsort(
-                -scores[relevant_items_partition])
-            ranking = relevant_items_partition[relevant_items_partition_sorting]
-            return str(ranking).strip("[]")
+        relevant_items_partition = (-scores).argpartition(n)[0:n]
+        relevant_items_partition_sorting = np.argsort(-scores[relevant_items_partition])
+        ranking = relevant_items_partition[relevant_items_partition_sorting]
+        return str(ranking).strip("[]")
