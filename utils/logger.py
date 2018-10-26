bec723906646d7aad3b1a9f44a0bd3c994a353c0
yigitozgumus <yigitozgumus1@gmail.com>
2018-10-26 19:08:32 +0200

    logging improved for fine tuning tracking

--- utils/logger.py
+++ utils/logger.py
@@ -1,14 +1,16 @@
 from time import asctime
-
+from pathlib import Path
 import os 
 class Logger(object):
 
-    def __init__(self, data, verbose=False):
+    def __init__(self, data, verbose=False, logFile=None):
         super(Logger, self).__init__()
         self.verbose = verbose
+        self.logFile = logFile
         self.data = data
         self.submission_list = []
         self.time = asctime()
+        self.dir = "Logs/"
 
     def export_experiments(self, model_bundle):
         index = len(list(os.listdir("./experiments"))) -2
@@ -30,8 +32,25 @@ class Logger(object):
             f.write(str(playlist_id) + ',' + model.recommend(playlist_id, n=10,export=True) + '\n');
         f.close()
 
+    def createLogFile(self,filename):
+        f = open(self.dir + filename,"w+")
+        f.write("playlist_id,track_ids\n")
+        f.write(
+            "| Experiment Name | Date | Model Name | MAP | Precision | Recall | Parameters |Submission|\n")
+        f.write(
+            "|---              |---   |---         |---  |---        |---     |---         |---      |\n")
+        f.close()
+
     def log_experiment(self):
-        f = open("Logs.md", "a")
+        if self.logFile is None:
+            f = open(self.dir + "Logs.md", "a")
+        else:
+            logFile = Path(self.dir + self.logFile)
+            if logFile.is_file():
+                f = open(self.dir + self.logFile, "a")
+            else:
+                createLogFile(self.logFile)
+                f = open(self.dir + self.logFile, "a")
         for submission in self.submission_list:
             f.write("\n|[" + submission[0] + "](" + submission[1]+ ")|" + "|".join(list(map(str,submission[2:]))) + "||")
 
