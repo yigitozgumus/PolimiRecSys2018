fe4e2e40bc2cece83e3fd9efad75ad07cb3716af
Yigit Ozgumus <yigitozgumus1@gmail.com>
2018-10-27 15:19:30 +0200

    recommend function re placement

--- base/BaseRecommender_SM.py
+++ base/BaseRecommender_SM.py
@@ -11,28 +11,27 @@ class RecommenderSystem_SM(object):
         # self.sparse_weights = None
 
     def recommend(self, playlist_id, exclude_seen=True, n=None, export=False):
-        
+
         if n is None:
             n = self.URM_train.shape[1] - 1
 
         # compute the scores using the dot product
         if self.sparse_weights:
-            scores = self.W_sparse[playlist_id].dot(self.URM_train).toarray().ravel()
+            user_profile = self.URM_train[playlist_id]
+            scores = user_profile.dot(self.W_sparse).toarray().ravel()
             # print(scores)
         else:
             # scores = self.URM_train.T.dot(self.W[playlist_id])
             user_profile = self.URM_train.indices[
-                           self.URM_train.indptr[playlist_id]:self.URM_train.indptr[playlist_id + 1]]
+                self.URM_train.indptr[playlist_id]:self.URM_train.indptr[playlist_id + 1]]
             user_ratings = self.URM_train.data[
-                           self.URM_train.indptr[playlist_id]:self.URM_train.indptr[playlist_id + 1]]
+                self.URM_train.indptr[playlist_id]:self.URM_train.indptr[playlist_id + 1]]
             relevant_weights = self.W[user_profile]
             scores = relevant_weights.T.dot(user_ratings)
-       
+
         if self.normalize:
             # normalization will keep the scores in the same range
             # of value of the ratings in dataset
-            user_profile = self.URM_train[playlist_id]
-
             rated = user_profile.copy()
             rated.data = np.ones_like(rated.data)
             if self.sparse_weights:
@@ -48,7 +47,8 @@ class RecommenderSystem_SM(object):
             scores = self._filter_seen_on_scores(playlist_id, scores)
 
         relevant_items_partition = (-scores).argpartition(n)[0:n]
-        relevant_items_partition_sorting = np.argsort(-scores[relevant_items_partition])
+        relevant_items_partition_sorting = np.argsort(
+            -scores[relevant_items_partition])
         ranking = relevant_items_partition[relevant_items_partition_sorting]
         if not export:
             return ranking
