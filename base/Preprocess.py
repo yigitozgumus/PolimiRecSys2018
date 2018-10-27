6719e49d50e4a0cc66d4fcb6e3fdf2398e192499
Yigit Ozgumus <yigitozgumus1@gmail.com>
2018-10-28 00:21:06 +0200

    first extraction is implemented

--- base/Preprocess.py
+++ base/Preprocess.py
@@ -8,7 +8,15 @@ def extractTrackPopularity(trainData,trackData):
     trackCounts = trainData['track_id'].value_counts().reset_index().rename(columns={"index":"track_id","track_id":"track_counts"})
     trackCounts = trackCounts.sort_values('track_counts',ascending=False)
     featureMatrix =  pd.merge(trackData,trackCounts,on='track_id')
+    featureMatrix = featureMatrix.drop(['duration_sec', 'album_id', 'artist_id'], axis=1)
     featureMatrix = sps.coo_matrix(featureMatrix)
     ICM = check_matrix(featureMatrix,'csr')
     return ICM
+#TODO implement these 
+def extractAlbumPopularity():
+    pass
+
+def extractArtistPopularity():
+    pass
+
 
