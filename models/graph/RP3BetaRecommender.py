ff39063fefd4dc4c0337f5310f217e22c63a716f
Yigit Ozgumus <yigitozgumus1@gmail.com>
2018-12-01 22:51:59 +0100

    graph recommender models are added

--- models/graph/RP3BetaRecommender.py
+++ models/graph/RP3BetaRecommender.py
@@ -1,47 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import numpy as np
 import scipy.sparse as sps
 
 from sklearn.preprocessing import normalize
-
-from utils.OfflineDataLoader import OfflineDataLoader
 from base.BaseRecommender import RecommenderSystem
-from base.BaseRecommender_SM import RecommenderSystem_SM
 from base.RecommenderUtils import check_matrix, similarityMatrixTopK
 
-
+from base.BaseRecommender_SM import RecommenderSystem_SM
+from utils.OfflineDataLoader import OfflineDataLoader
 import time, sys
 
-class P3alphaRecommender(RecommenderSystem,RecommenderSystem_SM):
-    """ P3alpha recommender """
 
-    RECOMMENDER_NAME = "P3_Alpha_Recommender"
+class RP3betaRecommender(RecommenderSystem_SM, RecommenderSystem):
+    """ RP3beta recommender """
+
+    RECOMMENDER_NAME = "RP3_Beta_Recommender"
     def __init__(self, URM_train):
-        super(P3alphaRecommender, self).__init__()
+        super(RP3betaRecommender, self).__init__()
+
         self.URM_train = check_matrix(URM_train, format='csr', dtype=np.float32)
         self.sparse_weights = True
         self.parameters = None
-        self.dataset=None
-
 
     def __repr__(self):
-        return "P3alpha Recommender"
+        return "RP3Beta Recommender"
 
-    def fit(self, topK=100, alpha=1., min_rating=0, implicit=False, normalize_similarity=False,save_model=False,best_parameters=False):
+    def fit(self, alpha=1., beta=0.6, min_rating=0, topK=100, implicit=False, normalize_similarity=True,save_model=False,best_parameters=False):
+
         if best_parameters:
             m = OfflineDataLoader()
-            folder_alpha, file_alpha = m.get_parameter(self.RECOMMENDER_NAME)
-            self.loadModel(folder_path=folder_alpha,file_name=file_alpha)
+            print(self.RECOMMENDER_NAME)
+            folder_beta, file_beta = m.get_parameter(self.RECOMMENDER_NAME)
+            self.loadModel(folder_path=folder_beta,file_name=file_beta)
         else:
-            self.topK = topK
             self.alpha = alpha
+            self.beta = beta
+            self.topK = topK
             self.normalize_similarity = normalize_similarity
 
         self.min_rating = min_rating
         self.implicit = implicit
-        self.parameters = "alpha={}, min_rating={}, topk={}, implicit={}, normalize_similarity={})".format(self.alpha,
-                                                                            self.min_rating, self.topK, self.implicit,
-                                                                            self.normalize_similarity)
+        self.parameters = "alpha={}, beta={}, min_rating={}, topk={}, implicit={}, normalize_similarity={})".format(
+            self.alpha,
+            self.beta, self.min_rating, self.topK,
+            self.implicit, self.normalize_similarity)
+
+        # if X.dtype != np.float32:
+        #     print("RP3beta fit: For memory usage reasons, we suggest to use np.float32 as dtype for the dataset")
 
         if self.min_rating > 0:
             self.URM_train.data[self.URM_train.data < self.min_rating] = 0
@@ -49,15 +56,26 @@ class P3alphaRecommender(RecommenderSystem,RecommenderSystem_SM):
             if self.implicit:
                 self.URM_train.data = np.ones(self.URM_train.data.size, dtype=np.float32)
 
-        #Pui is the row-normalized urm
+        # Pui is the row-normalized urm
         Pui = normalize(self.URM_train, norm='l1', axis=1)
 
-        #Piu is the column-normalized, "boolean" urm transposed
+        # Piu is the column-normalized, "boolean" urm transposed
         X_bool = self.URM_train.transpose(copy=True)
         X_bool.data = np.ones(X_bool.data.size, np.float32)
-        #ATTENTION: axis is still 1 because i transposed before the normalization
+
+        # Taking the degree of each item to penalize top popular
+        # Some rows might be zero, make sure their degree remains zero
+        X_bool_sum = np.array(X_bool.sum(axis=1)).ravel()
+
+        degree = np.zeros(self.URM_train.shape[1])
+
+        nonZeroMask = X_bool_sum != 0.0
+
+        degree[nonZeroMask] = np.power(X_bool_sum[nonZeroMask], -self.beta)
+
+        # ATTENTION: axis is still 1 because i transposed before the normalization
         Piu = normalize(X_bool, norm='l1', axis=1)
-        del(X_bool)
+        del (X_bool)
 
         # Alfa power
         if self.alpha != 1.:
@@ -78,7 +96,6 @@ class P3alphaRecommender(RecommenderSystem,RecommenderSystem_SM):
 
         numCells = 0
 
-
         start_time = time.time()
         start_time_printBatch = start_time
 
@@ -91,7 +108,7 @@ class P3alphaRecommender(RecommenderSystem,RecommenderSystem_SM):
             similarity_block = similarity_block.toarray()
 
             for row_in_block in range(block_dim):
-                row_data = similarity_block[row_in_block, :]
+                row_data = np.multiply(similarity_block[row_in_block, :], degree)
                 row_data[current_block_start_row + row_in_block] = 0
 
                 best = row_data.argsort()[::-1][:self.topK]
@@ -108,13 +125,11 @@ class P3alphaRecommender(RecommenderSystem,RecommenderSystem_SM):
                         cols = np.concatenate((cols, np.zeros(dataBlock, dtype=np.int32)))
                         values = np.concatenate((values, np.zeros(dataBlock, dtype=np.float32)))
 
-
                     rows[numCells] = current_block_start_row + row_in_block
                     cols[numCells] = cols_to_add[index]
                     values[numCells] = values_to_add[index]
 
                     numCells += 1
-
 
             if time.time() - start_time_printBatch > 60:
                 print("Processed {} ( {:.2f}% ) in {:.2f} minutes. Rows per second: {:.0f}".format(
@@ -128,17 +143,15 @@ class P3alphaRecommender(RecommenderSystem,RecommenderSystem_SM):
 
                 start_time_printBatch = time.time()
 
-        self.W_sparse = sps.coo_matrix((values[:numCells], (rows[:numCells], cols[:numCells])), shape=(Pui.shape[1], Pui.shape[1]))
-        self.W_sparse = check_matrix(self.W_sparse,"csr",dtype=np.float64)
-
+        self.W = sps.csr_matrix((values[:numCells], (rows[:numCells], cols[:numCells])),
+                                shape=(Pui.shape[1], Pui.shape[1]))
 
         if self.normalize_similarity:
-            self.W_sparse = normalize(self.W_sparse, norm='l1', axis=1)
-
+            self.W = normalize(self.W, norm='l1', axis=1)
 
         if self.topK != False:
-            self.W_sparse = similarityMatrixTopK(self.W_sparse, forceSparseOutput = True, k=self.topK)
+            self.W_sparse = similarityMatrixTopK(self.W, forceSparseOutput=True, k=self.topK)
             self.sparse_weights = True
 
         if save_model:
-            self.saveModel("saved_models/submission/",file_name="P3_Alpha_Recommender_submission_model")
+            self.saveModel("saved_models/submission/",file_name="RP3_Beta_Recommender_submission_model")
